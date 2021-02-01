8d352efe7463ddd66f524e90d0b4583103076120
Angelo Carvalho <angelosuporteti@gmail.com>
2021-02-01 13:58:10 -0300

    basic information

--- LogicaDeProgramacao/Program.cs
+++ LogicaDeProgramacao/Program.cs
@@ -6,7 +6,20 @@ namespace LogicaDeProgramacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Operadores aritmeticos em C, C++, Java e C# são: + - * / %(o operador % na linguagem de programação não e porcentagem é resto da divisão chamado mod)
+
+            //Dicas do VS: Identação automática CTRL + k + d, Rodar projeto CTRL +F5, Fonte CTRL + Mouse Scroll
+
+            // Declaração de variáveis:  <tipo> <nome> = <valor inicial>;
+            //Uma variável possui Nome, tipo, valor e Endereço
+            //No nome de uma variável não pode ter acentos, espaço e nem ser iniciada com número no inicio, o ideal é adotar o CamelCase
+            //Exemplos:
+            int idade = 31;
+            double altura = 1.68;
+            char sexo = 'F';
+
+            //As três operações básicas em programação são: Entrada de dados(leitura), processamento e saida(escrita)
+
         }
     }
 }
