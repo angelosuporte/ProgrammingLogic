e89a4ddb83134bf217698b37b4f4f1edf69e16a2
Angelo Carvalho <angelosuporteti@gmail.com>
2021-02-02 12:35:54 -0300

    Assignment command and casting

--- LogicaDeProgramacao/Program.cs
+++ LogicaDeProgramacao/Program.cs
@@ -7,26 +7,59 @@ namespace LogicaDeProgramacao
     {
         static void Main(string[] args)
         {
+            // Data processing
+            // Assignment command. Syntax: <variable> = <expression>
 
-            Console.WriteLine("Alô mundo");
-            Console.Write("Ei aluno,  ");
+            // Examples below
 
-            // Variáveis
-            //Entrada de dados 
-            string Nome = "estou aprendendo com voçê!";
-            int idade = 17;
-            double altura = 1.685;
+            Console.WriteLine("Example 1");
+            int a, b;
 
-            //Saída de dados
-            Console.WriteLine(Nome);
-            Console.WriteLine(idade);
-            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+            a = 3;
+            b = 2 * a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
 
-            //Concatatenação
+            Console.WriteLine("Example 2");
+            int c = 3;
+            double d = 2.0 * c;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
 
-            Console.WriteLine("Não sei meu nome ainda, mas " + Nome + "Tenho "  + idade + "minutos de idade. " 
-                + "Sou seu primeiro programa, mas se fosse uma pessoa, gostaria de crescer "
-                + altura.ToString("F2", CultureInfo.InvariantCulture) + " de altura");
+            Console.WriteLine("Example 3");
+            // calculate trapezoid where C = 12, D = 16, e = 10
+            double C, D, e, area;
+
+            C = 12.0;                               
+            D = 16.0;                                
+            e = 10.0;                               
+            area = (C + D) / 2.0 * e;
+            Console.WriteLine(area);
+
+            Console.WriteLine("Example 4");
+            //Example of casting
+            int E, f;
+            double total;
+
+            E = 3;
+            f = 2;
+
+            total = E / f;
+            Console.WriteLine(total);
+
+            Console.WriteLine("Calculate same example with casting");
+            // with casting
+            int g, F;
+            double result;
+
+            E = 3;
+            f = 2;
+
+            result = (double) E / f;
+            Console.WriteLine(result);
+
+
+
 
             Console.ReadKey();
             
