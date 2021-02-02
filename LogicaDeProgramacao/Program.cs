0532d52c6ff87c9f569df86196314ed96dc0b489
Angelo Carvalho <angelosuporteti@gmail.com>
2021-02-02 14:12:09 -0300

    Data input simple and with Split,

--- LogicaDeProgramacao/Program.cs
+++ LogicaDeProgramacao/Program.cs
@@ -7,58 +7,30 @@ namespace LogicaDeProgramacao
     {
         static void Main(string[] args)
         {
-            // Data processing
-            // Assignment command. Syntax: <variable> = <expression>
+            // data input
 
-            // Examples below
+            Console.WriteLine(" Good morning, you can start:");
+            string phrase = Console.ReadLine();
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
 
-            Console.WriteLine("Example 1");
-            int a, b;
-
-            a = 3;
-            b = 2 * a;
+            Console.WriteLine(phrase);
             Console.WriteLine(a);
             Console.WriteLine(b);
-
-            Console.WriteLine("Example 2");
-            int c = 3;
-            double d = 2.0 * c;
             Console.WriteLine(c);
+
+            // with split
+
+            Console.WriteLine("Input three in place of the x, as an example: xxxxx xxxxx xxxxxx ");
+            string[] vet = Console.ReadLine().Split(" ");
+            string d = vet[0];
+            string e = vet[1];
+            string f = vet[2];
+
             Console.WriteLine(d);
-
-            Console.WriteLine("Example 3");
-            // calculate trapezoid where C = 12, D = 16, e = 10
-            double C, D, e, area;
-
-            C = 12.0;                               
-            D = 16.0;                                
-            e = 10.0;                               
-            area = (C + D) / 2.0 * e;
-            Console.WriteLine(area);
-
-            Console.WriteLine("Example 4");
-            //Example of casting
-            int E, f;
-            double total;
-
-            E = 3;
-            f = 2;
-
-            total = E / f;
-            Console.WriteLine(total);
-
-            Console.WriteLine("Calculate same example with casting");
-            // with casting
-            int g, F;
-            double result;
-
-            E = 3;
-            f = 2;
-
-            result = (double) E / f;
-            Console.WriteLine(result);
-
-
+            Console.WriteLine(e);
+            Console.WriteLine(f);
 
 
             Console.ReadKey();
