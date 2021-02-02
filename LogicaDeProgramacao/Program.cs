abce0b25c1e3b47180d0363f387f0e578f314c4b
Angelo Carvalho <angelosuporteti@gmail.com>
2021-02-02 15:30:40 -0300

    Type conversion

--- LogicaDeProgramacao/Program.cs
+++ LogicaDeProgramacao/Program.cs
@@ -7,30 +7,29 @@ namespace LogicaDeProgramacao
     {
         static void Main(string[] args)
         {
-            // data input
+            // praticte: read one whole number, one letter, one double number. With Split read one name, sex, age and height with the right types
 
-            Console.WriteLine(" Good morning, you can start:");
-            string phrase = Console.ReadLine();
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            string c = Console.ReadLine();
+            Console.WriteLine("Hello, you can start:");
 
-            Console.WriteLine(phrase);
+
+            int a = int.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            double c1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(" ");
+            string name = vet[0];
+            char sex = char.Parse(vet[1]);
+            int age = int.Parse(vet[2]);
+            double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
             Console.WriteLine(a);
             Console.WriteLine(b);
-            Console.WriteLine(c);
+            Console.WriteLine(c1.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(name);
+            Console.WriteLine(sex);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
 
-            // with split
-
-            Console.WriteLine("Input three in place of the x, as an example: xxxxx xxxxx xxxxxx ");
-            string[] vet = Console.ReadLine().Split(" ");
-            string d = vet[0];
-            string e = vet[1];
-            string f = vet[2];
-
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            Console.WriteLine(f);
 
 
             Console.ReadKey();
