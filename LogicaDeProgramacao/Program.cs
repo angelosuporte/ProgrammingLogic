039e53d847d7b1cb27ef7232401f73d128c723b5
Angelo Carvalho <angelosuporteti@gmail.com>
2021-02-15 15:36:25 -0300

    Completed exercise 1005 Online judge

--- LogicaDeProgramacao/Program.cs
+++ LogicaDeProgramacao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace LogicaDeProgramacao
 {
@@ -6,14 +7,18 @@ namespace LogicaDeProgramacao
     {
         static void Main(string[] args)
         {
+            //Media ponderada = peque as notas * cada peso, e some os dois produtos, depois pegue a soma dos produtos divide pela soma dos pesos
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pesoA = 3.5;
+            double pesoB = 7.5;
 
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
+            double somaNotas = (A * pesoA) + (B * pesoB);
+            double media = somaNotas / (pesoA + pesoB);
 
-            int PROD = n1 * n2;
+            Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
 
-            Console.WriteLine("PROD = " + PROD);
-           
+            Console.ReadLine();
         }
     }
 }
